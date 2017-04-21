5000841edf9864a60e8bf660c21622210a2b7bfd
Jenkins <ci-softonic@softonic.com>
2017-04-21 11:01:12 +0000

    Updated SDK to 2.9.0

--- src/Configuration.php
+++ src/Configuration.php
@@ -109,7 +109,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'Swagger-Codegen/2.8.0/php';
+    protected $userAgent = 'Swagger-Codegen/2.9.0/php';
 
     /**
      * Debug switch (default set to false)
@@ -700,7 +700,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    OpenAPI Spec Version: 2.0.276-1690' . PHP_EOL;
-        $report .= '    SDK Package Version: 2.8.0' . PHP_EOL;
+        $report .= '    SDK Package Version: 2.9.0' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
